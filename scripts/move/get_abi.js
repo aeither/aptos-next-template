5ff1b73e35360b95d27873e26d54994dc81bc159
aeither <36173828+aeither@users.noreply.github.com>
2024-09-03 15:53:13 +0200

    add abi

--- scripts/move/get_abi.js
+++ scripts/move/get_abi.js
@@ -2,7 +2,11 @@ require('dotenv').config()
 const axios = require('axios')
 const fs = require('node:fs')
 
-const moduleName = 'aptos_friend'
+const ABI_FILE_PATH = 'src/utils/abi.ts'
+
+const files = fs.readdirSync('move/sources')
+const moveFile = files.find((file) => file.endsWith('.move'))
+const moduleName = moveFile.split('.')[0]
 
 const url = `https://fullnode.${process.env.NEXT_PUBLIC_APP_NETWORK}.aptoslabs.com/v1/accounts/${process.env.NEXT_PUBLIC_MODULE_ADDRESS}/module/${moduleName}`
 
@@ -12,8 +16,8 @@ async function getAbi() {
 		.then((response) => {
 			const abi = response.data.abi
 			const abiString = `export const ABI = ${JSON.stringify(abi)} as const;`
-			fs.writeFileSync('frontend/utils/abi.ts', abiString)
-			console.log('ABI saved to frontend/utils/abi.ts')
+			fs.writeFileSync(ABI_FILE_PATH, abiString)
+			console.log(`ABI saved to ${ABI_FILE_PATH}`)
 		})
 		.catch((error) => {
 			console.error('Error fetching ABI:', error)
