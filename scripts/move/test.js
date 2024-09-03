58c9ffb4c439970ee1aa0a5356063843e2b8ebd5
aeither <36173828+aeither@users.noreply.github.com>
2024-09-03 15:30:24 +0200

    format

--- scripts/move/test.js
+++ scripts/move/test.js
@@ -1,15 +1,15 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const cli = require("@aptos-labs/ts-sdk/dist/common/cli/index.js");
+const cli = require('@aptos-labs/ts-sdk/dist/common/cli/index.js')
 
 async function test() {
-  const move = new cli.Move();
+	const move = new cli.Move()
 
-  await move.test({
-    packageDirectoryPath: "move",
-    namedAddresses: {
-      message_board_addr: "0x100",
-    },
-  });
+	await move.test({
+		packageDirectoryPath: 'move',
+		namedAddresses: {
+			message_board_addr: '0x100'
+		}
+	})
 }
-test();
+test()
