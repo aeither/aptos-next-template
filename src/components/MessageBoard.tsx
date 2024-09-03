eb956e17839f07b1f56ca442e9a7db65ec25d61a
aeither <36173828+aeither@users.noreply.github.com>
2024-09-03 23:12:32 +0200

    fix

--- src/components/MessageBoard.tsx
+++ src/components/MessageBoard.tsx
@@ -4,6 +4,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { useWriteMessage } from '@/hooks/entry/useWriteMessage'
 import { useViewMessageContent } from '@/hooks/view/useViewMessageContent'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
@@ -18,7 +19,9 @@ export function MessageBoard() {
 		register,
 		handleSubmit,
 		formState: { isSubmitting, isDirty, isValid }
-	} = useForm<z.infer<typeof formSchema>>()
+	} = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema)
+	})
 
 	const onSubmit = (values: z.infer<typeof formSchema>) => {
 		writeMessage.mutate({ message: values.message })
