50497566a4a836731cfc7a3a473283ba82f2df6c
aeither <36173828+aeither@users.noreply.github.com>
2024-09-03 22:16:44 +0200

    use surf view

--- src/components/MessageBoard.tsx
+++ src/components/MessageBoard.tsx
@@ -2,78 +2,27 @@
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-// Internal components
-import { writeMessage } from '@/entry-functions/writeMessage'
-import { toast } from '@/hooks/use-toast'
-import { aptosClient } from '@/utils/aptosClient'
-import { getMessageContent } from '@/view-functions/getMessageContent'
+import { useWriteMessage } from '@/hooks/writeMessage'
+import { useViewMessageContent } from '@/view-functions/getMessageContent'
 import { useWallet } from '@aptos-labs/wallet-adapter-react'
-import { useQuery, useQueryClient } from '@tanstack/react-query'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 export function MessageBoard() {
-	const { account, signAndSubmitTransaction } = useWallet()
-	const queryClient = useQueryClient()
-
-	const [messageContent, setMessageContent] = useState<string>()
+	const { account } = useWallet()
 	const [newMessageContent, setNewMessageContent] = useState<string>()
-
-	const { data } = useQuery({
-		queryKey: ['message-content', account?.address],
-		refetchInterval: 10_000,
-		queryFn: async () => {
-			try {
-				const content = await getMessageContent()
-
-				return {
-					content
-				}
-			} catch (error: any) {
-				toast({
-					variant: 'destructive',
-					title: 'Error',
-					description: error
-				})
-				return {
-					content: ''
-				}
-			}
-		}
-	})
+	const { data } = useViewMessageContent()
+	const writeMessage = useWriteMessage()
 
 	const onClickButton = async () => {
 		if (!account || !newMessageContent) {
 			return
 		}
-
-		try {
-			const committedTransaction = await signAndSubmitTransaction(
-				writeMessage({
-					content: newMessageContent
-				})
-			)
-			const executedTransaction = await aptosClient().waitForTransaction({
-				transactionHash: committedTransaction.hash
-			})
-			queryClient.invalidateQueries()
-			toast({
-				title: 'Success',
-				description: `Transaction succeeded, hash: ${executedTransaction.hash}`
-			})
-		} catch (error) {
-			console.error(error)
-		}
+		writeMessage.mutate({ message: newMessageContent })
 	}
-
-	useEffect(() => {
-		if (data) {
-			setMessageContent(data.content)
-		}
-	}, [data])
 
 	return (
 		<div className="flex flex-col gap-6">
-			<h4 className="text-lg font-medium">Message content: {messageContent}</h4>
+			<h4 className="text-lg font-medium">Message content: {data?.content}</h4>
 			New message{' '}
 			<Input
 				disabled={!account}
--- src/utils/aptosClient.ts
+++ src/utils/aptosClient.ts
@@ -4,12 +4,18 @@ import {
 	type FundRequest
 } from '@aptos-labs/aptos-faucet-client'
 import { Aptos, AptosConfig } from '@aptos-labs/ts-sdk'
+import { createSurfClient } from '@thalalabs/surf'
 
 const aptos = new Aptos(new AptosConfig({ network: NETWORK }))
+const surf = createSurfClient(aptos)
 
 // Reuse same Aptos instance to utilize cookie based sticky routing
 export function aptosClient() {
 	return aptos
+}
+
+export function surfClient() {
+	return surf
 }
 
 export async function callFaucet(
